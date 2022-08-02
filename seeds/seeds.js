11b018ff122991c0705fcfa9ad70e99e5996bf87
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-02 17:43:15 -0400

    updated seeds

---
+++ seeds/seeds.js
@@ -0,0 +1,23 @@
+
+const sequelize = require('../config/connection');
+const { User, Posts } = require('../models');
+
+const userInput = require('./userInput.json');
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Posts.bulkCreate(userInput, {
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
